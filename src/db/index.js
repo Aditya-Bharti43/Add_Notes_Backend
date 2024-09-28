9d82f43390808b67a8e0133168f8c45371c3a543
Aditya Bharti <bhartiaditya685@gmail.com>
2024-09-28 17:55:44 +0530

    updates

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ import {DB_NAME} from '../constants.js'
 const connectDB = async function () {
 
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`mongodb+srv://Aditya:aditya123@cluster0.x7jlj.mongodb.net/${DB_NAME}`)
         console.log(`MongoDB connected !! DB HOST ${connectionInstance.connection.host}`)
     } catch (error) {
           console.log('MongoDB connection failed ',error)
--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ connectDB()
         })
 
         app.listen(process.env.PORT || 8000,'0.0.0.0',()=>{
-            console.log(`Server running at port ${process.env.PORT}`)
+            console.log(`Server running at port ${8000}`)
         })
 
         // app.get('/',(req,res)=>{
